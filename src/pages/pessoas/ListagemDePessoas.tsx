601b74584e508d8c98e45ae7f4f13d0e8457aa3d
Jon Glazkov <jonathan_vmacedo@hotmail.com>
2022-12-26 12:25:27 -0300

    feat: Adicionado hook useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,27 +1,34 @@
 import { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 
-import { PessoasServices } from "../../shared/services/api/pessoas/PessoasServices";
-import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePagina";
+import { useDebounce } from "../../shared/hooks";
 import { FerramentasDaListagem } from "../../shared/components";
+import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePagina";
+import { PessoasServices } from "../../shared/services/api/pessoas/PessoasServices";
 
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams("");
+  const { debounce } = useDebounce(2000);
 
   const onSearch = useMemo(() => {
     return searchParams.get("busca") || "";
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasServices.getAll(1, onSearch)
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-        } else {
+
+    debounce(() => {
+      PessoasServices.getAll(1, onSearch)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+            return;
+          }
+
           console.log(result);
-        }
-      });
+        });
+    });
+
   }, [onSearch]);
 
   return (
---
+++ src/shared/hooks/UseDebounce.ts
@@ -0,0 +1,24 @@
+import { useCallback, useRef } from "react";
+
+export const useDebounce = (delay = 300, noDelayInFirstTime = true) => {
+  const isFirstTime = useRef(noDelayInFirstTime);
+  const debouncing = useRef<NodeJS.Timeout>();
+
+  const debounce = useCallback((func: () => void) => {
+    if (isFirstTime.current){
+      isFirstTime.current = false;
+      func();
+
+    } else {
+
+      if (debouncing.current) {
+        clearTimeout(debouncing.current);
+      }
+  
+      debouncing.current = setTimeout(() => func(), delay);
+    }
+
+  }, [delay]);
+  
+  return { debounce };
+};
